b68887da743a2eab89933f987f281711c168eed8
Alan Fleming <fleming79@gmail.com>
2025-07-20 14:45:50 +1000

    Fix __init__ missing import.

--- src/async_kernel/__init__.py
+++ src/async_kernel/__init__.py
@@ -2,7 +2,8 @@
 # Distributed under the terms of the Modified BSD License.
 
 from async_kernel._version import __version__, kernel_protocol_version, kernel_protocol_version_info
-from async_kernel.kernel import Kernel, KernelInterruptError, KernelName
+from async_kernel.asyncshell import KernelInterruptError
+from async_kernel.kernel import Kernel, KernelName
 from async_kernel.pending_result import PendingResult
 from async_kernel.thread_caller import ThreadCaller
 
